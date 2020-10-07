ad39fbf900929d4db381682618be026d5d3ab40b
Lensom <magurawork@gmail.com>
2020-10-07 22:51:50 +0300

    add header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { OptForm, Feature } from '../components';
+import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
@@ -6,6 +8,19 @@ import { FaqsContainer } from '../containers/faqs';
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited films, TV propgremmes and more</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
